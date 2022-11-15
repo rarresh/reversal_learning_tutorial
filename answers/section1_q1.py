d04bb6871446c1bde0bb74e0d7fa497c5d5f20ef
Renée Koolschijn <40203491+rskool@users.noreply.github.com>
2022-11-15 16:09:22 +0100

    progress

---
+++ answers/section1_q1.py
@@ -0,0 +1,16 @@
+"""
+When β = 0, the choice probability is not affected by the value of the stimuli and always exactly at chance level.
+When β = ∞, choices become completely deterministic and the subject always picks the option with the maximum value.
+Mathematically:
+
+p(A) = exp(0 . Va) / (exp(0 . Va) + exp(0 . Vb)) = 1 / (1 + 1) = 0.5
+
+When β = 0, the values don't matter as they are multiplied by 0
+
+
+When β = ∞,
+
+
+So if VA >VB, even if this difference is tiny, then exp(∞ x VA))>> exp(∞ x VB), and so p(A)=1,
+vice versa when VB > VA.
+""";
---
+++ answers/section1_q2.py
@@ -0,0 +1,4 @@
+"""
+Because that would mean the subject would be more likely to pick the stimulus
+that has the lowest value! Now that would be a little silly...
+""";
